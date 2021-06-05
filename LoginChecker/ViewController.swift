6e346e14ac51f82deb0bdc3d1b23047e23e4f726
Sergey <a@a.com>
2021-06-05 15:14:02 +0700

    Initial Commit

---
+++ LoginChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginChecker
+//
+//  Created by Sergey Golovin on 05.06.2021.
+//  Copyright © 2021 GoldenWindGames LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
